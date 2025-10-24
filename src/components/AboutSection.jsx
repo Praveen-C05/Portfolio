48b298233a38bb40e4b657bc8ca1bcc69de34a2f
Mr.Praveen P <122772901+Praveen-C05@users.noreply.github.com>
2025-10-24 19:53:06 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -36,7 +36,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/public/projects/PRAVEEN P_resume.pdf"
+                href="/public/projects/PRAVEEN P.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
